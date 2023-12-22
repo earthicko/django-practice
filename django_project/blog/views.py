f5ff1a3c7fb63361ee6e656d1c297250b0957d55
earthicko <earthicko@gmail.com>
2023-12-22 14:55:12 +0900

    Feat: Part 3: add home, about templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -2,9 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+posts = [
+    {
+        "author": "이동현",
+        "title": "글 1",
+        "content": "첫번째 글",
+        "date_posted": "2023-12-22",
+    },
+    {
+        "author": "홍길동",
+        "title": "글 2",
+        "content": "두번째 글",
+        "date_posted": "2023-12-32",
+    },
+]
+
+
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {"posts": posts}
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, "blog/about.html", {"title": "About"})
