724f1c67a8f5ab58a31fdcaaf18254b90669ce03
earthicko <earthicko@gmail.com>
2023-12-25 17:11:05 +0900

    Feat: Part 5: delete dummy post data and utilize Post model data

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-posts = [
-    {
-        "author": "이동현",
-        "title": "글 1",
-        "content": "첫번째 글",
-        "date_posted": "2023-12-22",
-    },
-    {
-        "author": "홍길동",
-        "title": "글 2",
-        "content": "두번째 글",
-        "date_posted": "2023-12-32",
-    },
-]
+from .models import Post
 
 
 def home(request):
-    context = {"posts": posts}
+    context = {"posts": Post.objects.all()}
     return render(request, "blog/home.html", context)
 
 
