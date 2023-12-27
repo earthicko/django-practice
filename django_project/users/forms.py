0ba13fa600155a07ea9c7e24591499ee68abf5a2
earthicko <earthicko@gmail.com>
2023-12-27 19:49:35 +0900

    Feat: Part 9: add User and Profile update form and add views

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -9,3 +10,17 @@ class UserRegisterForm(UserCreationForm):
     class Meta:  # ModelForm을 상속하는 클래스는 해당 Model을 Meta class로 설정
         model = User  # sub-class 내의 변수이므로 이름 충돌 없음
         fields = ["username", "email", "password1", "password2"]
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ["username", "email"]
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["image"]
