3b0f8dbe8212cbc8880ef8cc7d5fbffa94431806
earthicko <earthicko@gmail.com>
2023-12-28 20:37:17 +0900

    Feat: Part 10

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,4 +1,13 @@
+from django.forms.models import BaseModelForm
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 from django.http import HttpResponse
 from .models import Post
 
@@ -6,6 +15,48 @@ from .models import Post
 def home(request):
     context = {"posts": Post.objects.all()}
     return render(request, "blog/home.html", context)
+
+
+class PostListView(ListView):
+    model = Post  # 리스트를 생성하기 위해 쿼리할 모델 지정
+    template_name = "blog/home.html"  # <app>/<model>_<view type>.html
+    context_object_name = "posts"  # def home()에서 사용된 key와 동일
+    ordering = ["-date_posted"]  # Post 모델의 애트리뷰트 이름
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ["title", "content"]
+
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ["title", "content"]
+
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = "/"
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
 
 
 def about(request):
